aaf52107e643b23c142509ba2f5e949cb92d0bb8
William Ryan <wcryan757@gmail.com>
2021-04-03 01:52:20 -0400

    Attempted assignment

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Your code here
+
+//const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown"), function(event) {
+    
+    if (event.key === "ArrowLeft") {
+    
+    const leftNumbers = dodger.style.left.replace("px", "");
+    
+    const left = parseInt(leftNumbers, 10);
+
+   
+    dodger.style.left = `${left - 1}px`; 
+};
+
+function moveDodgerLeft() {
+    
+    const leftNumbers = dodger.style.left.replace("px", "");
+    
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+    document.addEventListener("keydown"), function(e) {
+    if (e.key === "ArrowLeft") {
+
+    moveDodgerLeft();    
+    }
+        
+        
+    
+    
+};
+
+//Right:
+
+function moveDodgerRight() {
+    
+    const leftNumbers = dodger.style.left.replace("px", "");
+    
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}; 
+
+document.addEventListener("keydown"), function(e) {
+    
+    const rightNumbers = dodger.style.right.replace("px", "");
+    
+    const right = parseInt(rightNumbers, 10);
+
+    if (e.key === "ArrowRight") {
+        
+        
+        
+        moveDodgerRight();
+    }
+};
